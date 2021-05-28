e45f942538208a8497482b080d21f82593142c47
Josephh Carita <joseph.carita@tecsup.edu.pe>
2021-05-28 16:46:59 -0700

    Ultimos cambios para finalizar

--- Snapchat/iniciarSesionViewController.swift
+++ Snapchat/iniciarSesionViewController.swift
@@ -7,9 +7,25 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class iniciarSesionViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func IniciarSesion(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password:  passwordTextField.text!){ (user, error) in
+            print("Ententando Iniciar Sesion")
+            if error != nil {
+                print("Se presento el siguiente error: \(error) ")
+            }else {
+                print("Inicio de sesion exitoso")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
